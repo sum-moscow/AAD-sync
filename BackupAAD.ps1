bdf7bebe64b80edfa0423ed4c3a0073664b270a1
Zubarev Alexander <actionstrike@gmail.com>
2015-02-24 02:00:48 +0300

    Init
    Init

---
+++ BackupAAD.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.DESCRIPTION
+   This script export (backup) basic user Azure AD (Office 365) parameters to xml
+
+.AUTHOR    Zubarev Alexander aka Strike (av_zubarev@guu.ru)
+.COMPANY   State University of Management aka GUU
+   
+.LINK
+   https://github.com/sum-moscow/AAD-sync
+#>
+
+. "C:\AAD\loginOffice365.ps1"
+
+#to backup dir
+$BackupDir = $C.backupPath
+$date = Get-Date -Format 'yyyy.MM.dd'
+Set-Location $BackupDir
+New-Item -Type Directory -Name $date
+Set-Location $date
+
+Get-MsolUser -all | Export-Clixml "MsolUser.xml"
